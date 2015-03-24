df71d648398bbbfdfeaa9d3390867d7e38ca61a9
Akash Shakdwipeea <akash@debian>
2015-03-24 16:13:46 +0530

    Nodetime

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-require('newrelic');
+require('nodetime').profile({
+    accountKey: '23733758db654b03b03fa724c74ed2488313fb34', 
+    appName: 'Node.js Application'
+  });
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
