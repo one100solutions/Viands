efeeeb3a963ae2ef19d233d4c7ee9600d892410e
Akash Shakdwipeea <ashakdwipeea@gmail.com>
2015-04-04 15:08:32 +0530

    testing jenkins

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Viands' });
+  res.render('index', { title: 'Viands Api' });
 });
 
 module.exports = router;
