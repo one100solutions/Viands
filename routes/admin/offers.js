1febbd541842592caea078cb7e9b27026cc0e768
Akash Shakdwipeea <ashakdwipeea@gmail.com>
2015-07-12 03:40:28 +0530

    MAde copy s3 a utility

--- routes/admin/offers.js
+++ routes/admin/offers.js
@@ -6,6 +6,7 @@ var Offer = mongoose.model('Offer');
 var Banner = mongoose.model('Banner');
 
 var fs = require('fs');
+var upload = require('../../lib/copyS3');
 
 var AWS = require('aws-sdk');
 
@@ -20,67 +21,51 @@ AWS.config.loadFromPath(__dirname + '/../../aws_config.json');
 router.post('/createOffer', function  (req, res, next) {
 	var file = req.files.file;
 	req.body = JSON.parse(req.body.data);
+		console.log('Files', req.files)
+	console.log('Data', req.body)
 	/**
 	
 		TODO:
 		- Send pic to s3
 	**/
 
-	var stream = fs.createReadStream(file.path);
-
-	stream.on('error', function  (err) {
-		if (err) {
+	upload.put(file.path, 'offers/' + file.name, function  (err, msg) {
+		if(err) {
 			res.json({
 				err: true,
 				msg: err
 			})
-		} 
-	})
+		}
 
-	stream.on('open', function  () {
-		// body...
-		var s3 = new AWS.S3();
-		s3.putObject({
-			Bucket: 'viands',
-			Key: 'offers/' + file.name,
-			Body: stream
-		}, function  (err) {
-			if (err) {
+		else {
+			var b = new Offer();
+		
+			b.name = req.body.name;
+			b.logo = 'offers/' + file.name;
+			b.desc = req.body.desc;
+			b.points = req.body.points;
+			b.terms = req.body.terms;
+			b.url = req.body.url;
+
+			b.save(function  (err, b) {
+
+				if (err) {
+					res.json({
+						err: true,
+						msg: err
+					})
+				}else {
+
 				res.json({
-					err: true,
-					msg: err
+					err: false,
+					msg: 'Done',
+					banner: b
 				})
 			}
+			});
+		}
+	})
 
-			else {
-				var b = new Offer();
-			
-				b.name = req.body.name;
-				b.logo = 'offers/' + file.name;
-				b.desc = req.body.desc;
-				b.points = req.body.points;
-				b.terms = req.body.terms;
-				b.url = req.body.url;
-
-				b.save(function  (err, b) {
-
-					if (err) {
-						res.json({
-							err: true,
-							msg: err
-						})
-					}else {
-
-					res.json({
-						err: false,
-						msg: 'Done',
-						banner: b
-					})
-				}
-				});
-			}
-		})
-	});
 })
 
 /**
@@ -101,34 +86,16 @@ router.post('/createBanner', function  (req, res) {
 		- Send pic to s3
 	**/
 
-	var stream = fs.createReadStream(file.path);
-
-	stream.on('error', function  (err) {
-		if (err) {
+	upload.put(file.path, 'banners/' + file.name , function  (err, msg) {
+		if(err) {
 			res.json({
 				err: true,
 				msg: err
 			})
-		} 
-	})
+		}
 
-	stream.on('open', function  () {
-		// body...
-		var s3 = new AWS.S3();
-		s3.putObject({
-			Bucket: 'viands',
-			Key: 'banners/' + file.name,
-			Body: stream
-		}, function  (err) {
-			if (err) {
-				res.json({
-					err: true,
-					msg: err
-				})
-			}
-
-			else {
-				var b = new Banner();
+		else {
+			var b = new Banner();
 			
 				b.name = req.body.name;
 				b.url =  req.body.url;
@@ -150,9 +117,8 @@ router.post('/createBanner', function  (req, res) {
 					})
 				}
 				});
-			}
-		})
-	});
+		}
+	})
 
 	
 })
