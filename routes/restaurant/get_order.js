bf960d0289a6138a9b1a61891e37b7948f6a904e
Akash Shakdwipeea <ashakdwipeea@gmail.com>
2015-08-09 16:25:36 +0530

    Order later done

--- routes/restaurant/get_order.js
+++ routes/restaurant/get_order.js
@@ -1,5 +1,5 @@
 (function() {
-  var Order, Restaurant, express, mongoose, router;
+  var Order, Restaurant, express, mongoose, router, moment;
 
   express = require('express');
 
@@ -8,6 +8,8 @@
   mongoose = require('mongoose');
 
   Order = mongoose.model('Order');
+
+  moment = require('moment');
 
   Restaurant = mongoose.model('Restaurant');
 
@@ -33,6 +35,32 @@
               });
             } else if (orders) {
               console.log('Orders receive', orders)
+
+              //filter records take only what is applicable 
+              var now = new moment().add(5, 'hours').add(30, 'minutes');
+              console.log("Now",now.format());
+
+              //order can come before 10 minutes
+              //now.subtract(10,'minutes');
+
+              //filter the orders
+              orders = orders.filter(function  (order) {
+                if (order.type === 'later') {
+                  var order_time = new moment(order.time_deliver,'HH:mm').add(5, 'hours').add(30, 'minutes').subtract(10,'minutes');
+                  console.log("Order",order_time.format())
+                  if (order_time.isBefore(now) || order_time.isSame(now)) {
+                    return true;
+                  } else {
+                    return false;
+                  }  
+                } else {
+                  return true;
+                }
+                
+              })
+
+              console.log("After filter", orders)
+
               return res.json({
                 err: false,
                 message: 'Orders found',
